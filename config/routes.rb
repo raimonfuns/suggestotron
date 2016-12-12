c19de3c7d954db1129a3bd6f9dd925955ea9421b
raimonfuns <raimonfuns@163.com>
2016-12-12 10:47:55 +0800

    add downvote action

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	root 'topics#index'
+  resources :topics do
+  	member do
+  		post 'upvote'
+  		post 'downvote'
+  	end
+  end
+
+  get 'about', to: 'about#index'
 end
